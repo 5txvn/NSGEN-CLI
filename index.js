70943943395b43660cfd5fbafd914549e2dc1cf2
Steven <61475919+5txvn@users.noreply.github.com>
2024-03-04 14:49:50 -0600

    Add final touches for AP submission

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const chalk = require('chalk');
 //set the user score and streak
 let score = 0;
 let streak = 0;
-const times = [];
-let questions = 0;
+let times = [];
 console.log(chalk.bold("Welcome to NSGENv1.0.0: Type help for a list of commands"))
 
 function handleCommand() {
@@ -19,6 +18,7 @@ function handleCommand() {
       ${chalk.green("quit")} - exits the generation and brings you back to the command interface
       ${chalk.red("codes")} - lists the codes for problem generation
       ${chalk.blue("time")} - get the average time it took to answer all the problems from the session
+      ${chalk.green("cleartimes")} - clears all recorded times for problems
     `)
     handleCommand()
   } 
@@ -39,11 +39,6 @@ function handleCommand() {
       ▪️ ${chalk.red("x50")} - Generate a multiplying by 50 trick
       ▪️ ${chalk.red("x75")} - Generate a multiplying by 75 trick
       
-      ${chalk.bold("Memorization Tricks")}
-      ▪️ ${chalk.blue("squares")} - Generate some squares 1-40 to answer
-      ▪️ ${chalk.blue("cubes")} - Generate some cubes 1-20 to answer
-      ▪️ ${chalk.blue("fractions")} - Generate some fractions 1/2 - 1/12 to convert to decimals
-      
       ${chalk.bold("Remainder Tricks")}
       ▪️ ${chalk.green("r2")} - Generate a problem to find remainder dividing by 2
       ▪️ ${chalk.green("r3")} - Generate a problem to find remainder dividing by 3
@@ -60,10 +55,20 @@ function handleCommand() {
     times.forEach(time => {
       totalTime += time
     });
-    console.log(totalTime / questions);
+    if (times.length == 0) {
+      console.log("You have not completed any problems yet!")
+    } else {
+      console.log(`Your average time per question is ${chalk.bold(Math.round(totalTime / times.length) / 1000)} seconds`)
+    }
     handleCommand();
-  } else {
-    console.log("Invalid command");
+  } else if (command == "cleartimes") {
+    times = []
+    console.log("Times cleared!");
+    handleCommand();
+  }
+  
+  else {
+    console.log("Invalid command, type help for list of valid commands");
     handleCommand()
   }
 }
@@ -71,80 +76,90 @@ function handleCommand() {
 //call the command handler for the first time
 handleCommand()
 
-function generateNum(min, max) {
-  return Math.floor(Math.random() * (max-min)) + min;
+function generateNum(min, max, divisibility) {
+  if (!divisibility || divisibility == 0) {
+    return Math.floor(Math.random() * (max-min)) + min;
+  } else {
+    let num = generateNum(min, max, 0)
+    while (num % divisibility != 0) {
+      num = generateNum(min, max, 0)
+    }
+    return num
+  }
 }
 
-function generateRawProblem(num, string, equation) {
-  problem = prompt(string);
-  if (problem == equation) {
+function generateRawProblem(numParams, string, equation) {
+  let num = generateNum(numParams[0], numParams[1], numParams[2])
+  let time = Date.now()
+  problem = prompt(string.replace("num", num));
+  if (problem == eval(equation.replace("num", num))) {
     score++
     streak++
-    console.log(`Correct, your score is ${score}, and you streak is ${streak}`);
-    times.push(time);
-    clearInterval(increaseTime);
-    questions++;
-    generateProblem(code)
+    console.log(`${chalk.green.bold("Correct!")} Your score is ${chalk.green.bold(score)}, and you streak is ${chalk.yellow.bold(streak)}`);
+    times.push(Date.now() - time);
+    generateRawProblem(numParams, string, equation)
   } else if (problem == "quit") {
     handleCommand()
   } else {
     streak = 0;
-    console.log(`Incorrect! The answer is ${num*11}, and your streak has been reset`);
-    times.push(time);
-    clearInterval(increaseTime);
-    questions++;
-    generateProblem(code)
+    console.log(`${chalk.red.bold("Incorrect!")} The answer is ${eval(equation.replace("num", num))}, and your streak has been reset`);
+    times.push(Date.now() - time);
+    generateRawProblem(numParams, string, equation)
   }
 }
 
 function generateProblem(code) {
-  let time = 0;
-  const increaseTime = setInterval(function () {time++}, 1);
   let num;
   let problem
   switch(code) {
-      
-    //multiplying by 11
     case "x11":
-      /*
-      num = generateNum(10, 10000);
-      problem = prompt(`${num} x 11 = `);
-      if (problem == num*11) {
-        score++
-        streak++
-        console.log(`Correct, your score is ${score}, and you streak is ${streak}`);
-        times.push(time);
-        clearInterval(increaseTime);
-        questions++;
-        generateProblem(code)
-      } else if (problem == "quit") {
-        handleCommand()
-      } else {
-        streak = 0;
-        console.log(`Incorrect! The answer is ${num*11}, and your streak has been reset`);
-        times.push(time);
-        clearInterval(increaseTime);
-        questions++;
-        generateProblem(code)
-      }
-      */
-      num = generateNum(10, 10000);
-      generateRawProblem(num, `${num} x 11 = `, num*11);
+      generateRawProblem([10, 10000, 0], "num x 11 = ", "num*11");
       break;
-      
-    //multiplying by 12
     case "x12":
-      num = generateNum(10, 1000);
-      problem = prompt(`${num} x 12 = `);
-      if (problem == num*12) {
-        score++
-        streak++
-        console.log(`Correct, your score is ${score}, and you streak is ${streak}`);
-      } else {
-        streak = 0;
-        console.log(`Incorrect! The answer is ${num*12}, and your streak has been reset`);
-      }
-      generateProblem(code)
+      generateRawProblem([10, 1000, 0], "num x 12 = ", "num*12");
+      break;
+    case "x15":
+      generateRawProblem([20, 100, 0], "num x 15 = ", "num*15");
+      break;
+    case "x25":
+      generateRawProblem([10, 150, 0], "num x 25 = ", "num*25");
+      break;
+    case "x33-1/3":
+      generateRawProblem([10, 200, 3], "num x 33 1/3 = ", "num/3*100");
+      break;
+    case "x37":
+      generateRawProblem([10, 200, 3], "num x 37 = ", "num*37")
+      break;
+    case "x50":
+      generateRawProblem([10, 150, 0], "num x 50 = ", "num*50");
+      break;
+    case "x75":
+      generateRawProblem([10, 100, 0], "num x 75 = ", "num*75");
+      break;
+    //remainder problem generation
+    case "r2":
+      generateRawProblem([1000, 100000], "num / 2 remainder of... - ", "num%2");
+      break;
+    case "r3":
+      generateRawProblem([1000, 100000], "num / 3 remainder of... - ", "num%3");
+      break;
+    case "r4":
+      generateRawProblem([1000, 100000], "num / 4 remainder of... - ", "num%4");
+      break;
+    case "r5":
+      generateRawProblem([1000, 100000], "num / 5 remainder of... - ", "num%5");
+      break
+    case "r8":
+      generateRawProblem([1000, 100000], "num / 8 remainder of... - ", "num%8");
+      break;
+    case "r9":
+      generateRawProblem([1000, 100000], "num / 9 remainder of... - ", "num%9");
+      break;
+    case "r10":
+      generateRawProblem([1000, 100000], "num / 10 remainder of... - ", "num%10");
+      break;
+    case "r11":
+      generateRawProblem([1000, 100000], "num / 11 remainder of... - ", "num%11");
       break;
       
     default:
