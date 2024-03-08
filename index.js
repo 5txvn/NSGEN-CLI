1ce6d4aaa27bc881421e4c31c16d0ecd989b1ef8
Steven <61475919+5txvn@users.noreply.github.com>
2024-03-08 14:10:23 -0600

    Final commit with all the comments and completely functioning code

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-//set up the nodejs prompt
+ //import nodejs prompt lib
 const prompt = require('prompt-sync')();
-//set up chalk for terminal coloring
+//import chalk for terminal coloring
 const chalk = require('chalk');
-//set the user score and streak
+//set the user score, streak, and array of times per question
 let score = 0;
 let streak = 0;
 let times = [];
+//welcome message
 console.log(chalk.bold("Welcome to NSGENv1.0.0: Type help for a list of commands"))
 
+//function to handle command inputs from users
 function handleCommand() {
   let command = prompt("Enter a command: ");  
   //help command
   if (command == "help") {
     console.log(`
       ${chalk.red("help")} - get a list of commands
-      ${chalk.blue("generate")} - prompts the user to enter a code to ${chalk.bold("start problem generation")}
+      ${chalk.blue("generate")} - prompts the user to enter a code to start problem generation
       ${chalk.green("quit")} - exits the generation and brings you back to the command interface
       ${chalk.red("codes")} - lists the codes for problem generation
       ${chalk.blue("time")} - get the average time it took to answer all the problems from the session
@@ -22,12 +24,13 @@ function handleCommand() {
     `)
     handleCommand()
   } 
-  //generation command
+  //problem generation command
   else if (command == "generate") {
+    //ask user for generation code and generate problem
     let code = prompt("Enter a code: ");
-    let answer = prompt(`${generateProblem(code)[0]}`)
+    generateProblem(code)
   } 
-  //list codes command
+  //command to list all the codes for problem generation
   else if (command == "codes") {
     console.log(`
       ${chalk.bold("Multiplication Tricks")}
@@ -52,22 +55,27 @@ function handleCommand() {
     handleCommand()
   } else if (command == "time") {
     let totalTime = 0;
+    //add each time to the sum of the times to find the average
     times.forEach(time => {
       totalTime += time
     });
+    //if the times array is empty, print that there are no recorded times
     if (times.length == 0) {
       console.log("You have not completed any problems yet!")
-    } else {
+    } 
+    //print average time if the array is populated
+    else {
       console.log(`Your average time per question is ${chalk.bold(Math.round(totalTime / times.length) / 1000)} seconds`)
     }
     handleCommand();
   } else if (command == "cleartimes") {
+    //reset the times array to clear all the recorded times
     times = []
     console.log("Times cleared!");
     handleCommand();
   }
-  
   else {
+    //handler if the user enters a command that doesn't exist
     console.log("Invalid command, type help for list of valid commands");
     handleCommand()
   }
@@ -76,6 +84,7 @@ function handleCommand() {
 //call the command handler for the first time
 handleCommand()
 
+//generate a number that also includes a divisibility parameter if a generated number needs to be divisible by a certain number, such as with the multiplying by 37 trick
 function generateNum(min, max, divisibility) {
   if (!divisibility || divisibility == 0) {
     return Math.floor(Math.random() * (max-min)) + min;
@@ -88,30 +97,48 @@ function generateNum(min, max, divisibility) {
   }
 }
 
+//generate the actual raw problem based off parameters that describe the mix, max, and divisibility of the number that needs to be generated, the string outputted to the user in the console when running the program, and also the equation used to evaluate whether the answer the user inputted was correct or not
 function generateRawProblem(numParams, string, equation) {
+  //generate the number to be used
   let num = generateNum(numParams[0], numParams[1], numParams[2])
+  //set time at the start of the problem
   let time = Date.now()
+  //prompt the user with a problem
   problem = prompt(string.replace("num", num));
+  //handler if the user got the problem correct
   if (problem == eval(equation.replace("num", num))) {
+    //increment the score and streak
     score++
     streak++
+    //send message to user
     console.log(`${chalk.green.bold("Correct!")} Your score is ${chalk.green.bold(score)}, and you streak is ${chalk.yellow.bold(streak)}`);
+    //add the time it took to answer the problem to the times array by subtracting the current Date.now() time from the initial time
     times.push(Date.now() - time);
+    //recursive function, acts as the loop to keep the function going until the user decides to quit
     generateRawProblem(numParams, string, equation)
-  } else if (problem == "quit") {
+  } 
+  //handler if the user wants to exit the loop
+  else if (problem == "quit") {
     handleCommand()
-  } else {
+  } 
+  //handler if the user gets the problem wrong
+  else {
+    //reset streak
     streak = 0;
+    //output error message to user
     console.log(`${chalk.red.bold("Incorrect!")} The answer is ${eval(equation.replace("num", num))}, and your streak has been reset`);
+    //add time it took the user to answer the problem to the times array
     times.push(Date.now() - time);
+    //regenerate function
     generateRawProblem(numParams, string, equation)
   }
 }
 
+//function to generate the actual problem with a case statement to define the function parameters for the generateRawProblem function based off the code that the user inputted;
 function generateProblem(code) {
-  let num;
-  let problem
+  //case statement to handle the different codes that the user can input
   switch(code) {
+    //problem generation for the multiplication problems
     case "x11":
       generateRawProblem([10, 10000, 0], "num x 11 = ", "num*11");
       break;
@@ -136,7 +163,7 @@ function generateProblem(code) {
     case "x75":
       generateRawProblem([10, 100, 0], "num x 75 = ", "num*75");
       break;
-    //remainder problem generation
+    //problem generation for the remainder problems
     case "r2":
       generateRawProblem([1000, 100000], "num / 2 remainder of... - ", "num%2");
       break;
@@ -161,7 +188,7 @@ function generateProblem(code) {
     case "r11":
       generateRawProblem([1000, 100000], "num / 11 remainder of... - ", "num%11");
       break;
-      
+    //if the code is not valid, print error message and prompt the user to re-enter a command
     default:
       console.log("Invalid code, try again")
       handleCommand()
